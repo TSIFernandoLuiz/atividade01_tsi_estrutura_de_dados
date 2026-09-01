e36552852f48e476d3995d577fff134cb8a9d619
luizfernandorodriguesTSI <163867380+TSIFernandoLuiz@users.noreply.github.com>
2026-08-31 21:44:56 -0300

    Algoritmo25
    O algoritmo recebe uma data no formato DDMMAA e separa dia, mês e ano.

--- Algoritmo25.java
+++ Algoritmo25.java
@@ -1,15 +1,22 @@
 import java.util.Scanner;
+
 public class Algoritmo25 {
+    
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
+        
         System.out.print("Digite a data no formato DDMMAA: ");
         int data = scanner.nextInt();
+        
         int dia = data / 10000;
         int mes = (data % 10000) / 100;
         int ano = data % 100;
+        
         System.out.println("DIA: " + dia);
         System.out.println("MES: " + mes);
         System.out.println("ANO: " + ano);
+        
         scanner.close();
     }
 }
