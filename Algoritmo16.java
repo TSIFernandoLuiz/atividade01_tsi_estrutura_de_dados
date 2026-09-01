e60741ae9bb87d66228e6caa9cd173eafac222b6
luizfernandorodriguesTSI <163867380+TSIFernandoLuiz@users.noreply.github.com>
2026-08-31 21:42:24 -0300

    Algoritmo16
    O algoritmo 16 declara uma variável inteira, atribui 10 a ela e imprime seu conteúdo.

--- Algoritmo16.java
+++ Algoritmo16.java
@@ -1,7 +1,12 @@
 public class Algoritmo16 {
+    
     public static void main(String[] args) {
+        
         int x;
+        
         x = 10;
+        
         System.out.println(x);
+        
     }
 }
